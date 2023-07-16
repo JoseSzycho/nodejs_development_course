89793a612e9bdb6e80d9056c23fe24fbedac19eb
Jose <jszychowski.laba@solvd.com>
2023-07-16 16:50:38 -0300

    Create tests for multply string prototype

---
+++ 01_class/tests/multiplyStringPrototype.test.js
@@ -0,0 +1,27 @@
+const generateRandomNumber =  require('../scripts/generateRandomNumber'); //it generates a random number with a max number of digits
+const StringPrototype = require('../scripts/multiplyStringPrototype'); 
+const maxDigitNumber = 40; //set the max number of digit for the random number (number of digits will be from 1 to maxDigitNumber)
+const numberOfTests = 100; //number of test to be performed
+
+for(let i = 0; i < numberOfTests; i ++){
+
+    //generating random numbers with a random amount of digits
+    let multiplicand = generateRandomNumber(maxDigitNumber);
+    let multiplier = generateRandomNumber(maxDigitNumber);
+
+    //result using BigInt
+    let resultMathLib = String(BigInt(multiplicand) * BigInt(multiplier)); 
+
+    //result usign multiply prototype of String
+    let resultStringMultiply = multiplicand.multiply(multiplier); 
+
+    describe('Test multiply prototype and type of result', () => {
+        test(`Expect ${multiplicand} * ${multiplier} to be ${resultMathLib}`, () => {
+            expect(resultStringMultiply).toBe(resultMathLib);
+        });
+
+        test(`Expect result to be string`, () => {
+            expect(typeof resultStringMultiply).toBe("string");
+        });
+    });
+}
