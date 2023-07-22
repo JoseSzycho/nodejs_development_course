2092296bdb49fef28a7b4dcb20c2412a5ead49f5
Jose Szychowski <jszychowski.laba@solvd.com>
2023-07-21 22:04:04 -0300

    Complete and test function

--- 03_class/homework/tests/addValues.test.js
+++ 03_class/homework/tests/addValues.test.js
@@ -1,5 +1,42 @@
-import {addValues} from '../index.js';
+const addValues = require('../utils/addValues');
 
-test("Expect", () => {
-    expect(addValues(1,2)).toBe(3);
-})
+describe('Testing error scenarios', () => {
+
+    test('One value is a NaN', () => {
+        expect(() => {
+            addValues(5, NaN)})
+            .toThrow(TypeError)
+    });
+
+    test('One value is a undefined', () => {
+        expect(() => {
+            addValues(undefined, "hey")})
+            .toThrow(TypeError)
+    });
+
+    test('Values are valid data types but are different types', () => {
+        expect(() => {
+            addValues(5, "hey")})
+            .toThrow(TypeError)
+    });
+
+    test('One value is an array', () => {
+        expect(() => {
+            addValues(5, [5])})
+            .toThrow(TypeError)
+    });
+});
+
+describe('Testing functional escenarios', () => {
+
+    test('Expect 5 + 82 to be 87', () => {
+        expect(addValues(5, 82))
+        .toBe(87);
+    });
+
+    test('Expect "foo" + "bar" to be "foobar', () => {
+        expect(addValues('foo', 'bar'))
+        .toBe('foobar');
+    });
+
+});
--- 03_class/homework/utils/addValues.js
+++ 03_class/homework/utils/addValues.js
@@ -1,3 +1,29 @@
+// Adds to values.
+// It only work with String and Number
+// Only returns if both values are equal type
+
 const addValues = (value1, value2) => {
-    return value1 + value2;
+    
+    // Function that throw error if data type is not valid
+    const checkType = (value) => {
+        if(typeof value == 'string') return; // valid data type
+        if(typeof value == 'number') {
+           if(!(value == value)) throw new TypeError('NaN is not valid for adding.');
+           return; // valid data type
+        }
+        
+        // If here, it is not a valid data type
+        throw new TypeError('Value must be a number or string');
+    }
+
+    // Cecking if errror must be thrown
+    checkType(value1);
+    checkType(value2);
+    if(typeof(value1) != typeof(value2)) throw new TypeError('Both values must be equal type.');
+    
+    if(typeof value1 == 'number') return value1 + value2;
+    if(typeof value2 == 'string') return value1.concat(value2);
+ 
 }
+
+module.exports = addValues;
