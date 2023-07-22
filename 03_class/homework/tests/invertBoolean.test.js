8d1f2f63ebd839496ea5b58c8455881e325490a5
Jose Szychowski <jszychowski.laba@solvd.com>
2023-07-22 15:15:37 -0300

    Create test

---
+++ 03_class/homework/tests/invertBoolean.test.js
@@ -0,0 +1,51 @@
+const invertBoolean = require('../utils/invertBoolean');
+
+test('Expect false to be true', () => {
+    expect(invertBoolean(false)).toBe(true);
+});
+
+test('Expect true to be false', () => {
+    expect(invertBoolean(true)).toBe(false);
+});
+
+test('Expect number 0 to be error', () => {
+    expect(() => {
+        invertBoolean(0)})
+        .toThrow('Value must be boolean type.');
+});
+
+test('Expect number 1 to be error', () => {
+    expect(() => {
+        invertBoolean(1)})
+        .toThrow('Value must be boolean type.');
+});
+
+test('Expect string "1" to be error', () => {
+    expect(() => {
+        invertBoolean('1')})
+        .toThrow('Value must be boolean type.');
+});
+
+test('Expect string "0" to be error', () => {
+    expect(() => {
+        invertBoolean('0')})
+        .toThrow('Value must be boolean type.');
+});
+
+test('Expect array [] to be error', () => {
+    expect(() => {
+        invertBoolean('[]')})
+        .toThrow('Value must be boolean type.');
+});
+
+test('Expect array [1] to be error', () => {
+    expect(() => {
+        invertBoolean('[1]')})
+        .toThrow('Value must be boolean type.');
+});
+
+test('Expect array [0] to be error', () => {
+    expect(() => {
+        invertBoolean('[0]')})
+        .toThrow('Value must be boolean type.');
+});
