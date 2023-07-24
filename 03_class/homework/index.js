3632929aae3dde8169214e1764484d76af5ab836
Jose Szychowski <jszychowski.laba@solvd.com>
2023-07-23 22:04:46 -0300

    Create export library file

---
+++ 03_class/homework/index.js
@@ -0,0 +1,13 @@
+const {addValues} = require('./utils/addValues');
+const coerceToType = require('./utils/coerceToType')
+const convertToNumber = require('./utils/convertToNumber');
+const invertBoolean = require('./utils/invertBoolean');
+const stringifyValue = require('./utils/stringifyValue');
+
+module.exports = {
+    addValues, 
+    coerceToType, 
+    convertToNumber, 
+    invertBoolean, 
+    stringifyValue
+};
