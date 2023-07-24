0bb89ee88b9b9feffe6d55cd030ff6f92df380ff
Jose Szychowski <jszychowski.laba@solvd.com>
2023-07-24 11:30:37 -0300

    Modify to use ! operator for inverting result

--- 03_class/homework/utils/invertBoolean.js
+++ 03_class/homework/utils/invertBoolean.js
@@ -1,11 +1,6 @@
 const invertBoolean = (value) => {
     if(typeof value !== 'boolean') throw new TypeError('Value must be boolean type.');
-    if(value === true) return false;
-    if(value === false) return true;
-
-    // If here, there is a bug
-    throw new TypeError(`Unexpected error with ${value} of type ${typeof value},
-             copy paste this error and send to developer.`);
+    return !value;
 }
 
 module.exports = invertBoolean;
