339660e81a31750b8c32de8f936712e481f5aa00
Jose Szychowski <jszychowski.laba@solvd.com>
2023-07-28 19:02:40 -0300

    Add comments

--- 05_class/homework/task2/getFullName.js
+++ 05_class/homework/task2/getFullName.js
@@ -3,12 +3,15 @@ const getFullName = ({ firstName, lastName }) => {
     return el.charAt(0).toUpperCase() + el.slice(1).toLowerCase();
   };
 
+  // If we have more that one name we split it
   const separatedFirstNames = firstName.split(" ");
+  // And we map to have the normalized names
   const normalizedFirstName = separatedFirstNames.map(normalizeName);
 
   const separatedLastNames = lastName.split(" ");
   const normalizedLastName = separatedLastNames.map(normalizeName);
 
+  //Names must be joined, they were trheated as arrays.
   return `${normalizedFirstName.join(" ")} ${normalizedLastName.join(" ")}`;
 };
 
