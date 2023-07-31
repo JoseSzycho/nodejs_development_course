7c906adf781f24bb129c8aa9596a3812c5896049
Jose Szychowski <jszychowski.laba@solvd.com>
2023-07-31 16:59:52 -0300

    Add error input error checking

--- 05_class/homework/task1/calculateTotalPrice.js
+++ 05_class/homework/task1/calculateTotalPrice.js
@@ -1,5 +1,5 @@
 const calculateTotalPrice = (priceOfProducts) => {
-  const errorIfNotValidPrice = (el) => {
+  const errorIfNotValidPrice = (el) => { 
     if (typeof el != "number")
       throw new TypeError("Cannot operate over this element.");
     if (el < 0) throw new TypeError("Cannot operate with negative numbers.");
