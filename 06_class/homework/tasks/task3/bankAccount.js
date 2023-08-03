7003fd83deb95e2440eb767434187570c4d18110
Jose Szychowski <jszychowski.laba@solvd.com>
2023-08-03 20:43:41 -0300

    Complete task 3

--- 06_class/homework/tasks/task3/bankAccount.js
+++ 06_class/homework/tasks/task3/bankAccount.js
@@ -1,14 +1,72 @@
+///////////
+// IT could be great to make this exercise using function constructor,
+//  as the transfer function is perfect for being a prototype,
+//  and using "instance of" I could easily check if a bank account is
+//  the correct type for making the transfer
+//
+// As it is stated to use "Setter and Getter" I understand we must use object literal.
+//////////
+
+// Function that check if a number is positive and >= 0
+const checkIfValidNumber = (errorMessage) => {
+  return (value) => {
+    if (!(typeof value === "number" && value >= 0))
+      throw new TypeError(errorMessage);
+  };
+};
+
+// checking valid number with custom message
+const checkForNegativeBalance = checkIfValidNumber(
+  "Balance cannot be negative."
+);
+
+// checking valid number with custom message
+const checkForInvalidValue = checkIfValidNumber(
+  "New value must be a number and >= 0."
+);
+
+// This function calculates which bankAccount argument is the sender.
+// The sender will be the bankAccout that is calling the transfer method
+const setRemitentAndTarget = (pointer, bankAccount1, bankAccount2) => {
+  if (bankAccount1 == bankAccount2)
+    throw new Error("Cannot transfer from the same bank account.");
+
+  if (pointer == bankAccount1) {
+    // If we are calling the method from bankAccount1, bankAccount2 is target
+    return [bankAccount1, bankAccount2];
+  } else if (pointer == bankAccount2) {
+    // If we are calling the method from bankAccount2, bankAccount1 is target
+    return [bankAccount2, bankAccount1];
+  }
+
+  throw new Error("Cannot transfer from a third account.");
+};
+
 const bankAccount = {
   _balance: 1000,
+  _formattedBalance: "$1000",
   get formattedBalance() {
-    return `$${this._balance}`;
+    return this._formattedBalance;
   },
   set balance(value) {
     // Checking for a valid number
-    if (!(typeof value === "number" && value >= 0))
-      throw new TypeError("New value must be a number and >= 0.");
+    checkForInvalidValue(value);
 
     this._balance = value;
+    this._formattedBalance = `$${value}`;
+  },
+  transfer(bankAccount1, bankAccount2, valueToTransfer) {
+    checkForInvalidValue(valueToTransfer);
+    const [currentAccount, targetAccount] = setRemitentAndTarget(
+      this,
+      bankAccount1,
+      bankAccount2
+    );
+
+    const currentAccountNewBalance = currentAccount._balance - valueToTransfer;
+    checkForNegativeBalance(currentAccountNewBalance); // If balance will be negative, throw an erro
+    currentAccount.balance = currentAccountNewBalance;
+    targetAccount.balance = targetAccount._balance + valueToTransfer;
   },
 };
 
--- 06_class/homework/tests/task3.test.js
+++ 06_class/homework/tests/task3.test.js
@@ -3,7 +3,7 @@ test("Expect get property formattedBalance to return $1000", () => {
   expect(bankAccount.formattedBalance).toBe("$1000");
 });
 
-test("Expect setting new balance value to update balance value", () => {
+test("Expect setting new balance value to update formated balance value", () => {
   const { bankAccount } = require("../tasks/task3/bankAccount");
   bankAccount.balance = 9834;
   expect(bankAccount.formattedBalance).toBe("$9834");
@@ -29,3 +29,58 @@ test("Expect string new balance value to throw an error", () => {
     "New value must be a number and >= 0."
   );
 });
+
+test("Transfer from account 1 to account 2", () => {
+  const { bankAccount } = require("../tasks/task3/bankAccount");
+  const bankAccount1 = Object.create(bankAccount);
+  const bankAccount2 = Object.create(bankAccount);
+  let valueToTransfer = 5000;
+
+  bankAccount1.balance = 9000;
+  bankAccount2.balance = 8000;
+
+  bankAccount1.transfer(bankAccount1, bankAccount2, valueToTransfer);
+
+  expect(bankAccount1.formattedBalance).toBe("$4000");
+  expect(bankAccount2.formattedBalance).toBe("$13000");
+
+  valueToTransfer = 1000;
+
+  // Switching order from bank accounts. bankAccount 1 should keep being the one that is transfering
+  bankAccount1.transfer(bankAccount2, bankAccount1, valueToTransfer);
+  expect(bankAccount1.formattedBalance).toBe("$3000");
+  expect(bankAccount2.formattedBalance).toBe("$14000");
+
+  
+});
+
+test("Expect error if an account argument is not the sending one", () => {
+  const { bankAccount } = require("../tasks/task3/bankAccount");
+  const bankAccount1 = Object.create(bankAccount);
+  const bankAccount2 = Object.create(bankAccount);
+  const bankAccount3 = Object.create(bankAccount);
+  let valueToTransfer = 5000;
+
+  bankAccount1.balance = 9000;
+  bankAccount2.balance = 8000;
+  bankAccount2.balance = 5000;
+
+  expect(() =>
+    bankAccount1.transfer(bankAccount2, bankAccount3, valueToTransfer)
+  ).toThrow("Cannot transfer from a third account.");
+});
+
+test("Expect error if account will be in negative balance after transfer", () => {
+  const { bankAccount } = require("../tasks/task3/bankAccount");
+  const bankAccount1 = Object.create(bankAccount);
+  const bankAccount2 = Object.create(bankAccount);
+  let valueToTransfer = 10000;
+
+  bankAccount1.balance = 9000;
+  bankAccount2.balance = 8000;
+
+  expect(() =>
+    bankAccount1.transfer(bankAccount1, bankAccount2, valueToTransfer)
+  ).toThrow("Balance cannot be negative.");
+});
+
