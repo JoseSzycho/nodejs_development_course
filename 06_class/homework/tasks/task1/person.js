9c1b2e7cec390d5aff884ec950b7179fb14b3dfe
Jose Szychowski <jszychowski.laba@solvd.com>
2023-08-03 16:59:41 -0300

    Move and rename file

---
+++ 06_class/homework/tasks/task1/person.js
@@ -0,0 +1,38 @@
+// For setting Writable descriptor property to true or false
+const setWritableDescriptor = (obj, key, value) => {
+  Object.defineProperty(obj, key, {
+    writable: value,
+  });
+};
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  email: "john.doe@example.com",
+  updateInfo: (newInfo) => {
+    Object.keys(person).forEach((key) => {
+      if (Object.keys(newInfo).includes(key)) {
+        // If a newFinfo property is not in person, we ignore it
+        setWritableDescriptor(person, key, true);
+        person[key] = newInfo[key];
+        setWritableDescriptor(person, key, false);
+      }
+    });
+  },
+};
+
+// Setting properties as no-writable (only read)
+Object.keys(person).forEach((key) => setWritableDescriptor(person, key, false));
+
+// Creating new property
+Object.defineProperty(person, "address", {
+  value: {},
+  writable: true,
+  enumerable: false,
+  configurable: false,
+});
+
+module.exports = {
+  person,
+};
