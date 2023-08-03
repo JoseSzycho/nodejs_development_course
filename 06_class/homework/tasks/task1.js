1d93d09137a6e2e3504c45be61c82792219507d7
Jose Szychowski <jszychowski.laba@solvd.com>
2023-08-03 16:17:54 -0300

    Create updateInfo method

--- 06_class/homework/tasks/task1.js
+++ 06_class/homework/tasks/task1.js
@@ -1,15 +1,27 @@
+const setWritableDescriptor = (obj, key, value) => {
+  Object.defineProperty(obj, key, {
+    writable: value,
+  });
+};
+
 const person = {
   firstName: "John",
   lastName: "Doe",
   age: 30,
   email: "john.doe@example.com",
+  updateInfo: (newInfo) => {
+    Object.keys(person).forEach((key) => {
+      if (Object.keys(newInfo).includes(key)) {
+        // If new info is not in person, we ignore it
+        setWritableDescriptor(person, key, true);
+        person[key] = newInfo[key];
+        setWritableDescriptor(person, key, false);
+      }
+    });
+  },
 };
 
-for (let key in person) {
-  Object.defineProperty(person, key, {
-    writable: false,
-  });
-}
+Object.keys(person).forEach((key) => setWritableDescriptor(person, key, false));
 
 module.exports = {
   person,
--- 06_class/homework/tests/task1.test.js
+++ 06_class/homework/tests/task1.test.js
@@ -1,4 +1,4 @@
-test("Expect properties to not be changed.", () => {
+test("Expect properties to not be changed", () => {
   const { person } = require("../tasks/task1");
   //Attempting to modify newPerson propertires
   person.firstName = "Homero";
@@ -10,4 +10,26 @@ test("Expect properties to not be changed.", () => {
   expect(person.lastName).toBe("Doe");
   expect(person.age).toBe(30);
   expect(person.email).toBe("john.doe@example.com");
+});
+
+test("Expect properties to ONLY be updated with updateInfo property", () => {
+  const { person } = require("../tasks/task1");
+
+  const newInfo = {
+    firstName: "Jane",
+    lastName: "Mary",
+    age: 32,
+    email: "mary.jane@example.com",
+    surname: "MaryJane",
+  };
+
+  person.updateInfo(newInfo);
+  person.name = "Jose"; // Name must not be changef with out updateInfo
+
+  expect(person.firstName).toBe("Jane");
+  expect(person.lastName).toBe("Mary");
+  expect(person.age).toBe(32);
+  expect(person.email).toBe("mary.jane@example.com");
+  expect(person.surname).toBe(undefined);
+  
 });
