22320c6dba442f902b57596abaebb6fd64f011a5
Jose Szychowski <jszychowski.laba@solvd.com>
2023-08-04 18:22:36 -0300

    Fix typo

--- 06_class/homework/tests/task5.test.js
+++ 06_class/homework/tests/task5.test.js
@@ -1,11 +1,11 @@
-const { observeObject } = require("../tasks/task5/observeObject");
+const { observedObject } = require("../tasks/task5/observedObject");
 const { person } = require("../tasks/task1/person");
 
 test("Expect called properties of task 1 object to be logged when accessed", () => {
   const logFunction = (prop, action) =>
     console.log(`Property "${prop}" performed a "${action}" action.`);
 
-  const proxyPerson = observeObject(person, logFunction);
+  const proxyPerson = observedObject(person, logFunction);
   proxyPerson.firstName = "Jose"; // => Property "name" performed a "set" action.
   proxyPerson.age; // =>  Property "age" performed a "get" action.
   proxyPerson.address = "Warsaw"; // =>  Property "address" performed a "set" action.
@@ -20,7 +20,7 @@ test("Expect observedObject to store and give values correctly", () => {
 
   const doNothing = () => {};
 
-  const proxyStudent = observeObject(student, doNothing);
+  const proxyStudent = observedObject(student, doNothing);
 
   proxyStudent.name = "Beth";
   proxyStudent.age = 25;
