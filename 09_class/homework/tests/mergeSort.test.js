0a68716b712b7e6bbd2e9355b9d73584091be690
Jose Szychowski <jszychowski.laba@solvd.com>
2023-08-22 23:21:05 -0300

    Modify

--- 09_class/homework/tests/mergeSort.test.js
+++ 09_class/homework/tests/mergeSort.test.js
@@ -1,7 +1,7 @@
 const { generateRandomArray } = require("../utils/generateRandomArray");
 const { mergeSort } = require("../utils/mergeSort");
 
-test.skip("Expect arrays to be ordered", () => {
+test("Expect arrays to be ordered", () => {
   // Performs a certain number of tests, in each test a random array will be generated
   // with the length of the current iteration of the for cycle.
   // Then we will compared our sort method with the sort method from js, and we expect
@@ -15,10 +15,3 @@ test.skip("Expect arrays to be ordered", () => {
     expect(mergeSortedArray).toEqual(jsSortedArray);
   }
 });
-
-test("Expect arrays to be ordered", () => {
-  const array = [3,4,2,1,6,8,6,2];
-  expect(mergeSort(array)).toEqual([1,2,2,3,4,6,6,8])
-});
-
-  
