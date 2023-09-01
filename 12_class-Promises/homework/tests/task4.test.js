c6128f4bb152c3113a237efdfd0f295d6c28d9d2
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-01 13:15:01 -0300

    Adds reject test

--- 12_class-Promises/homework/tests/task4.test.js
+++ 12_class-Promises/homework/tests/task4.test.js
@@ -1,5 +1,17 @@
 const { promisify } = require("../tasks/task4");
 
+function callbackStyleFunction(value, callback) {
+  setTimeout(() => {
+    if (value > 0) {
+      callback(null, value * 2);
+    } else {
+      callback("Invalid value", null);
+    }
+  }, 1000);
+}
+
+const promisedFunction = promisify(callbackStyleFunction);
+
 test("Expect for input error", () => {
   expect(() => promisify(1)).toThrow(
     "Input must be a function with two arguments."
@@ -8,4 +20,18 @@ test("Expect for input error", () => {
   expect(() => promisify((el) => console.log(el))).toThrow(
     "Input must be a function with two arguments."
   );
+});
+
+test("Expect promisify to resolve", async () => {
+  const result = await promisedFunction(3);
+
+  expect(result).toBe(6);
+});
+
+test("Expect promisify to reject", async () => {
+  try {
+    await promisedFunction(0);
+  } catch (error) {
+    expect(error.message).toBe("Invalid value");
+  }
 });
