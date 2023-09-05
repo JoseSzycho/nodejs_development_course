cd9c827c869bcb5918fa30d16cb831bc7685fb0a
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-05 17:23:02 -0300

    Add testing scenario

--- 13_class-Classes/homework/tests/Book.test.js
+++ 13_class-Classes/homework/tests/Book.test.js
@@ -1,4 +1,6 @@
 const { Book } = require("../classes/Book");
+const { Cart } = require("../classes/Cart");
+const { User } = require("../classes/User");
 
 test("Expect new instance of book to have correct property values", () => {
   const testBook = new Book()
@@ -14,5 +16,55 @@ test("Expect new instance of book to have correct property values", () => {
     ISBN: "ISBN Number",
     price: "Price",
     availability: "Available",
+  });
+});
+
+describe("Expect books availability to be updated correctly. Availability = 2", () => {
+  const testUser = new User()
+    .withName("Test Name")
+    .withEmail("Test email")
+    .withID("ID number");
+
+  const testUserCart = new Cart().withUser(testUser);
+
+  const testBook = new Book()
+    .withTitle("Test Title")
+    .withAuthor("Test Author")
+    .withISBN("ISBN Number")
+    .withPrice("Price")
+    .withAvailability(2);
+
+  test("Expect testBook availability to be 2", () => {
+    expect(testBook.availability).toBe(2);
+  });
+
+  test("Expect testBook availability to be 1 after book removal", () => {
+    testUserCart.addBook(testBook);
+    expect(testBook.availability).toBe(1);
+  });
+
+  test("Expect testBook availability to be 0 after book removal", () => {
+    testUserCart.addBook(testBook);
+    expect(testBook.availability).toBe(0);
+  });
+
+  test("Expect testBook availability to be 0 after book removal", () => {
+    testUserCart.addBook(testBook);
+    expect(testBook.availability).toBe(0);
+  });
+
+  test("Expect testBook availability to be 1 after book adding", () => {
+    testUserCart.removeBook(testBook);
+    expect(testBook.availability).toBe(1);
+  });
+
+  test("Expect testBook availability to be 2 after book adding", () => {
+    testUserCart.removeBook(testBook);
+    expect(testBook.availability).toBe(2);
+  });
+
+  test("Expect testBook availability to be 2 after book adding", () => {
+    testUserCart.removeBook(testBook);
+    expect(testBook.availability).toBe(2);
   });
 });
