4b08274827d59665800adaff24b168961cb1f279
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-05 16:35:24 -0300

    Add methods

--- 13_class-Classes/homework/classes/Cart.js
+++ 13_class-Classes/homework/classes/Cart.js
@@ -3,4 +3,34 @@ class Cart {
     this.user;
     this.booksList = [];
   }
+
+  addBook(book) {
+    if (book.availability > 0) {
+      book.availability -= 1;
+      this.booksList.push(book);
+      console.log("Book added to the cart.");
+    } else {
+      console.log("Book is not available.");
+    }
+  }
+
+  removeBook(book) {
+    const index = this.booksList.indexOf(book);
+    if (index === -1) {
+      console.log("Book is not present in the cart.");
+    } else {
+      book.availability += 1;
+      this.booksList.splice(index, 1);
+      console.log("Book removed from the cart.");
+    }
+  }
+
+  calculateTotalPrice() {
+    const totalPrice = this.booksList.reduce(
+      (acc, book) => acc + book.price,
+      0
+    );
+
+    return totalPrice;
+  }
 }
