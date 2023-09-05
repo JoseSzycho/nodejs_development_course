3d56bf1597aa710076c6bb05e5b1017436ec815d
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-05 19:00:19 -0300

    Fix test to fit new User property

--- 13_class-Classes/homework/tests/User.test.js
+++ 13_class-Classes/homework/tests/User.test.js
@@ -1,5 +1,6 @@
 const { User } = require("../classes/User");
 const { Cart } = require("../classes/Cart");
+const { Book } = require("../classes/Book");
 
 test("Expect new instance of book to have correct property values", () => {
   const testUser = new User()
@@ -12,5 +13,42 @@ test("Expect new instance of book to have correct property values", () => {
     email: "Test email",
     ID: "ID number",
     cart: new Cart(),
+  });
+});
+
+describe("Expect add and remove books to update Book object", () => {
+  const testBook1 = new Book()
+    .withTitle("Test Title")
+    .withAuthor("Test Author")
+    .withISBN("ISBN Number")
+    .withPrice(1)
+    .withAvailability(2);
+
+  const testBook2 = new Book()
+    .withTitle("Test Title")
+    .withAuthor("Test Author")
+    .withISBN("ISBN Number")
+    .withPrice(5)
+    .withAvailability(2);
+
+  const testUser = new User()
+    .withName("Test Name")
+    .withEmail("Test email")
+    .withID("ID number");
+
+  test("Expect testBook1 to update quantity", () => {
+    testUser.cart.addBook(testBook1);
+    expect(testBook1.availability).toBe(1);
+  });
+
+  test("Expect testBook2 to update quantity", () => {
+    testUser.cart.addBook(testBook2);
+    expect(testBook2.availability).toBe(1);
+  });
+
+  test("Expect total price to be calculated", () => {
+    testUser.cart.addBook(testBook1);
+    testUser.cart.addBook(testBook2);
+    expect(testUser.cart.calculateTotalPrice()).toBe(12);
   });
 });
