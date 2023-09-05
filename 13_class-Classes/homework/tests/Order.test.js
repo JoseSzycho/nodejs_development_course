9b9c35390c2eda2895e518fd314c00491d69de29
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-05 20:18:57 -0300

    Add test escenario

--- 13_class-Classes/homework/tests/Order.test.js
+++ 13_class-Classes/homework/tests/Order.test.js
@@ -2,30 +2,60 @@ const { User } = require("../classes/User");
 const { Order } = require("../classes/Order");
 const { Book } = require("../classes/Book");
 
-test("Expect order to be consoled", () => {
+const testBook1 = new Book()
+  .withTitle("Test Title 1")
+  .withAuthor("Test Author")
+  .withISBN("ISBN Number")
+  .withPrice(1)
+  .withAvailability(20);
+
+const testBook2 = new Book()
+  .withTitle("Test Title 2")
+  .withAuthor("Test Author")
+  .withISBN("ISBN Number")
+  .withPrice(10)
+  .withAvailability(20);
+
+test("Expect order to be shown", () => {
   const testUser = new User()
     .withName("Test Name")
     .withEmail("Test email")
     .withID("ID number");
 
-  const testBook1 = new Book()
-    .withTitle("Test Title 1")
-    .withAuthor("Test Author")
-    .withISBN("ISBN Number")
-    .withPrice(1)
-    .withAvailability(2);
+  testUser.cart.addBook(testBook1);
+  testUser.cart.addBook(testBook2);
 
-  const testBook2 = new Book()
-    .withTitle("Test Title 2")
-    .withAuthor("Test Author")
-    .withISBN("ISBN Number")
-    .withPrice(1)
-    .withAvailability(2);
+  const testOrder = new Order().withUser(testUser);
+  testOrder.show();
+});
+
+test("Expect order to be executed and cart to be cleaned up", () => {
+  const testUser = new User()
+    .withName("Test Name")
+    .withEmail("Test email")
+    .withID("ID number");
 
   testUser.cart.addBook(testBook1);
   testUser.cart.addBook(testBook2);
 
   const testOrder = new Order().withUser(testUser);
-
-  testOrder.show();
+  expect(testUser.cart.booksList).toEqual([testBook1, testBook2]);
+  testOrder.execute();
+  expect(testUser.cart.booksList).toEqual([]);
 });
+
+test("Expect order to be cancelled and cart to be cleaned up", () => {
+    const testUser = new User()
+      .withName("Test Name")
+      .withEmail("Test email")
+      .withID("ID number");
+  
+    testUser.cart.addBook(testBook1);
+    testUser.cart.addBook(testBook2);
+  
+    const testOrder = new Order().withUser(testUser);
+    expect(testUser.cart.booksList).toEqual([testBook1, testBook2]);
+    testOrder.cancel();
+    expect(testUser.cart.booksList).toEqual([]);
+  });
+  
