715411b3e94bca3ae2c65e1178fe1e2626172e13
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-07 10:30:25 -0300

    Comment code

--- 13_class-Classes/homework/classes/DataBase.js
+++ 13_class-Classes/homework/classes/DataBase.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 
+/** Class that allows to load pre-created data. The returned data
+ * is an array where is each element is an object with the
+ * desired data.
+ */
 class DataBase {
   #path;
   constructor() {
@@ -11,22 +15,39 @@ class DataBase {
    * @returns {this} - The reference to the data base object
    */
   withPath(path) {
-    console.log(path);
+    // Basic validation
     if (typeof path != "string") throw new Error("Invalid path type.");
+
     this.#path = path;
     return this;
   }
+  /**
+   * Load desired data from data base and transform it into array
+   * @private
+   * @param {string} desiredData - The desired data to obtain from data base
+   * @returns {Array} - The desired data
+   */
   #loadData(desiredData) {
     try {
+      // Reading database file
       const data = fs.readFileSync(this.#path, "utf-8");
-      return JSON.parse(data)[desiredData].map(el => el);
+      // Returns parsed data, filtered by desired data and converted to array.
+      return JSON.parse(data)[desiredData].map((el) => el);
     } catch (error) {
       throw new Error(error.message);
     }
   }
+  /**
+   * Gets users from data base
+   * @returns {Array} - The users data
+   */
   getUsers() {
     return this.#loadData("users");
   }
+  /**
+   * Gets books from data base
+   * @returns {Array} - The books data
+   */
   getBooks() {
     return this.#loadData("books");
   }
