2762b5d235808159d4b4e2348502cc5a816d773f
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-07 09:54:03 -0300

    Resume code

--- 13_class-Classes/homework/classes/DataBase.js
+++ 13_class-Classes/homework/classes/DataBase.js
@@ -3,26 +3,33 @@ const fs = require("fs");
 class DataBase {
   #path;
   constructor() {
-    this.#path = path;
+    this.#path;
   }
+  /**
+   * Sets data base path
+   * @param {string} path - The database path
+   * @returns {this} - The reference to the data base object
+   */
   withPath(path) {
+    console.log(path);
     if (typeof path != "string") throw new Error("Invalid path type.");
     this.#path = path;
     return this;
   }
-  #loadData() {
+  #loadData(desiredData) {
     try {
       const data = fs.readFileSync(this.#path, "utf-8");
-      return JSON.parse(data);
+      return JSON.parse(data)[desiredData].map(el => el);
     } catch (error) {
       throw new Error(error.message);
     }
   }
   getUsers() {
-    const users = this.#loadData.users.map((user) => user);
-    return users;
+    return this.#loadData("users");
   }
   getBooks() {
-    const books = this.#loadData.books.map((book) => book);
+    return this.#loadData("books");
   }
 }
+
+module.exports = { DataBase };
