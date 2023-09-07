f7573c1bc397b18049b4ee3ceac2613e0594f8f5
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-06 21:00:48 -0300

    Add comments

--- 13_class-Classes/homework/classes/User.js
+++ 13_class-Classes/homework/classes/User.js
@@ -1,10 +1,12 @@
 const { Cart } = require("../classes/Cart");
 
-// Template that represents a user
+/**
+ * Class representing an user
+ * */
 class User {
   #ID;
   #cart;
-  static #IDList = new Set(); // Stores ID to prevent duplicated users
+  static #IDList = new Set(); // Stores ID to prevent duplicated users */
   constructor() {
     this.name;
     this.email;
@@ -12,35 +14,68 @@ class User {
     this.#cart = new Cart();
   }
 
-  // Returns ID
+  /**
+   * Gets the user ID
+   * @returns {string} The user ID
+   */
   get ID() {
     return this.#ID;
   }
 
-  // Returns cart
+  /**
+   * Gets the user cart
+   * @returns {Cart} The user cart
+   */
   get cart() {
     return this.#cart;
   }
 
-  // Sets name after a verification
+  /**
+   * Sets the user name
+   * @param {string} name The user name
+   * @returns {this} The class reference
+   */
   withName(name) {
+    /* Basic pattern for names. It can cause problems for
+    some languages */
     const namePattern = /^[A-Za-z\s'-]+$/;
-    if (!namePattern.test(name))
-      throw new Error("Name is not valid.");
+
+    // Checking for valid input name
+    if (!namePattern.test(name)) throw new Error("Name is not valid.");
+
     this.name = name;
     return this;
   }
+
+  /**
+   * Sets the user email address
+   * @param {string} email The user email
+   * @returns The class reference
+   */
   withEmail(email) {
     // Basic email pattern. rfc2822 stands for complete pattern.
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // Checking for valid email input
     if (!emailPattern.test(email))
       throw new Error("E-mail address is not valid.");
+
     this.email = email;
     return this;
   }
+  /**
+   * Sets the users ID
+   * @param {string} ID The user ID
+   * @returns The class reference
+   */
   withID(ID) {
+    // Checking for valid input ID
+    if (typeof ID != "string") throw new Error("ID is not valid.");
+    // Checking for repeated ID
     if (User.#IDList.has(ID)) throw new Error("User ID already used.");
+
+    // Adding ID to users ID list
     User.#IDList.add(ID);
+
     this.#ID = ID;
     return this;
   }
