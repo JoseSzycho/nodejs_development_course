e794fc63b87acdf75a1d35e350fd46d4912a8404
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-07 22:11:36 -0300

    Add comments

--- 13_class-Classes/homework/demostration.js
+++ 13_class-Classes/homework/demostration.js
@@ -9,12 +9,16 @@ const { getBooksObjects } = require("./functions/getBooksObjects");
 
 const db = new DataBase().withPath("./data_base/data.json");
 
+// Getting array of parsed objects
 const usersData = db.getUsers();
 const booksData = db.getBooks();
 
+// Getting array of class instances for each array
+// of parsed objects
 const users = getUsersObjects(usersData, User);
 const books = getBooksObjects(booksData, Book);
 
+// Starts interactive scenario
 const navigation = new Navigation()
   .withUsers(users)
   .withBooks(books)
