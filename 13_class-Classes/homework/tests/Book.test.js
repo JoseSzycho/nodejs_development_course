7dcd5ac7c54a4a412c88478797e17f40719feb80
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-08 10:23:49 -0300

    Add tests to ensure correct category are setted

--- 13_class-Classes/homework/tests/Book.test.js
+++ 13_class-Classes/homework/tests/Book.test.js
@@ -1,10 +1,28 @@
-const { Book } = require("../classes/Book");
 const { Cart } = require("../classes/Cart");
+const { Book } = require("../classes/Book");
+const { ActionBook } = require("../classes/ActionBook");
+const { ThrillerBook } = require("../classes/ThrillerBook");
 
 let testBook;
+let actionBook;
+let thrillerBook;
 let testUserCart;
 beforeAll(() => {
   testBook = new Book()
+    .withTitle("Test Title")
+    .withAuthor("Test Author")
+    .withISBN("ISBN Number")
+    .withPrice(2)
+    .withAvailability(2);
+
+  actionBook = new ActionBook()
+    .withTitle("Test Title")
+    .withAuthor("Test Author")
+    .withISBN("ISBN Number")
+    .withPrice(2)
+    .withAvailability(2);
+
+  thrillerBook = new ThrillerBook()
     .withTitle("Test Title")
     .withAuthor("Test Author")
     .withISBN("ISBN Number")
@@ -18,6 +36,7 @@ test("Expect new instance of book to have correct property values", () => {
   expect(testBook).toEqual({
     title: "Test Title",
     author: "Test Author",
+    category: null,
     ISBN: "ISBN Number",
     price: 2,
     availability: 2,
@@ -57,5 +76,12 @@ describe("Expect books availability to be updated correctly. Availability = 2", 
   test("Expect testBook availability to be 2 after book adding", () => {
     testUserCart.removeBook(testBook);
     expect(testBook.availability).toBe(2);
+  });
+
+  test("Expect action book category to be 'action'", () => {
+    expect(actionBook.category).toBe("action");
+  });
+  test("Expect thriller book category to be 'thriller'", () => {
+    expect(thrillerBook.category).toBe("thriller");
   });
 });
