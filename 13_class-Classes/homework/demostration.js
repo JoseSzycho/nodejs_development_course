e1bdcb65f9aec5eabe5e15e004d70a0f02ed916d
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-08 10:23:12 -0300

    Add polymorphims demostration

--- 13_class-Classes/homework/demostration.js
+++ 13_class-Classes/homework/demostration.js
@@ -2,6 +2,8 @@ const readline = require("readline");
 const { User } = require("./classes/User");
 const { Order } = require("./classes/Order");
 const { Book } = require("./classes/Book");
+const { ActionBook } = require("./classes/ActionBook");
+const { ThrillerBook } = require("./classes/ThrillerBook");
 const { DataBase } = require("./classes/DataBase");
 const { Navigation } = require("./classes/Navigation");
 const { getUsersObjects } = require("./functions/getUsersObjects");
@@ -16,7 +18,7 @@ const booksData = db.getBooks();
 // Getting array of class instances for each array
 // of parsed objects
 const users = getUsersObjects(usersData, User);
-const books = getBooksObjects(booksData, Book);
+const books = getBooksObjects(booksData, Book, ActionBook, ThrillerBook);
 
 // Starts interactive scenario
 const navigation = new Navigation()
