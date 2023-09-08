493d281f1739f1999132587b3a0e6f326fb8e3cf
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-08 19:16:08 -0300

    Add comment

--- 14_class-Data_structure/homework/01_data_structure_implementations/stack/Stack.js
+++ 14_class-Data_structure/homework/01_data_structure_implementations/stack/Stack.js
@@ -9,10 +9,13 @@ class Stack {
     this.#stack = {};
     // Start in -1 so the index always point to last element,
     // as it increases when a element is pushed.
-    this.#index = -1; 
+    this.#index = -1;
   }
   /**
-   * Add an element at the top of the stack
+   * Add an element at the top of the stack.
+   *
+   * It works by incrementing the index and creating a new pair
+   * property/element with this index at the stack object.
    * @param {*} el The element
    */
   push(el) {
@@ -20,7 +23,13 @@ class Stack {
     this.#stack[this.#index] = el;
   }
   /**
-   * Removes last element from the stack and returns it
+   * Removes last element from the stack and returns it.
+   *
+   * It works by storing the last property value of the stack
+   * using the stored index. Then the property es deleted and the
+   * index is decreased. The index cannot be less than -1 so it can
+   * be prepared for storing the first stack element at index 0
+   * when pushing it if the stack is empty.
    * @returns {*} The element
    */
   pop() {
@@ -30,7 +39,10 @@ class Stack {
     return lastEl;
   }
   /**
-   * Returns the last element from the stack
+   * Returns the last element from the stack.
+   * 
+   * It works by returning the property value corresponding
+   * to current stack index.
    * @returns {*} The element
    */
   peek() {
