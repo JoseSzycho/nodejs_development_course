ad09370715710fd3dfa6fe8ec6c2f738f2057610
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-09 17:01:53 -0300

    Add comments

--- 14_class-Data_structure/homework/01_data_structure_implementations/linked_list/Node.js
+++ 14_class-Data_structure/homework/01_data_structure_implementations/linked_list/Node.js
@@ -1,15 +1,33 @@
+/**
+ * Class that creates a node structure with any kind of data.
+ * This structure is needed as #nextNode can be used to point
+ * to another node, so a linked list can be created.
+ */
 class Node {
   #nextNode;
+
+  /**
+   * Initialize the node with certain data.
+   * @param {*} data The data
+   */
   constructor(data) {
     this.data = data;
     this.#nextNode = null;
   }
 
+  /**
+   * Returns the #nextNode property
+   */
   get nextNode() {
     return this.#nextNode;
   }
 
+  /**
+   * Sets #nextNode with a new Node / null after a a input check
+   *@param {Node} - The Node
+   */
   set nextNode(node) {
+    // Checking input data
     if (node === null || node.constructor === Node) {
       this.#nextNode = node;
       return;
