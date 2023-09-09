9333ef76a14a5ae5dfc0c3b195e587ec128b8f26
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-09 11:36:21 -0300

    Make variables public for easier testing

--- 14_class-Data_structure/homework/01_data_structure_implementations/linked_list/Node.js
+++ 14_class-Data_structure/homework/01_data_structure_implementations/linked_list/Node.js
@@ -1,21 +1,21 @@
 class Node {
-  #data;
-  #nextNode;
+  data;
+  nextNode;
   constructor(data) {
-    this.#data = data;
-    this.#nextNode = null;
+    this.data = data;
+    this.nextNode = null;
   }
   get data() {
-    return this.#data;
+    return this.data;
   }
 
   get nextNode() {
-    return this.#nextNode;
+    return this.nextNode;
   }
 
   set nextNode(node) {
     if (node === null || node.constructor === Node) {
-      this.#nextNode = node;
+      this.nextNode = node;
       return;
     }
     throw new Error("nextNode must be null or a Node");
