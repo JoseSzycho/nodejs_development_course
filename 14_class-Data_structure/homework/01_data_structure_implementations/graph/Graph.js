8138d04407c1c5c9896b925c0a547f386a49787c
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-10 17:40:40 -0300

    Add addEdge and addVertex methods

---
+++ 14_class-Data_structure/homework/01_data_structure_implementations/graph/Graph.js
@@ -0,0 +1,31 @@
+class Graph {
+  constructor() {
+    this.vertices = new Set();
+    this.edges = {};
+  }
+
+  addVertex(vertex) {
+    if (this.vertices.has(vertex)) {
+      console.log("Vertex is already in the graph");
+    } else {
+      this.vertices.add(vertex);
+      this.edges[vertex] = new Set();
+      console.log("Vertex added.");
+    }
+  }
+  addEdge(fromVertex, toVertex) {
+    const vertices = this.vertices;
+    if (vertices.has(fromVertex) && vertices.has(toVertex)) {
+      this.edges[fromVertex].add(toVertex);
+      this.edges[toVertex].add(fromVertex);
+      console.log("Edge added.");
+    } else {
+      if (!vertices.has(fromVertex))
+        console.log(`${fromVertex} is not in the graph.`);
+      if (!vertices.has(toVertex))
+        console.log(`${toVertex} is not in the graph.`);
+    }
+  }
+}
+
+module.exports = { Graph };
