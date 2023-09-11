39eb9959c8ba41751a28d08d1fed5037d943aa26
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-11 18:03:41 -0300

    Add comments

--- 14_class-Data_structure/homework/02_algorithmetic_problems/linked_list_cycle/hasCycle.js
+++ 14_class-Data_structure/homework/02_algorithmetic_problems/linked_list_cycle/hasCycle.js
@@ -2,22 +2,40 @@ const {
   Node,
 } = require("../../01_data_structure_implementations/linked_list/Node");
 
+/**
+ * Functions checks if a linked list has a cycle.
+ *
+ * It works by using Floyd's Cycle Detection Algorithm.
+ *
+ * @param {Node} node The node
+ * @returns {Boolean} __true__ if it has a cycle, __false__ if not.
+ */
 const hasCycle = (node) => {
+  // Checking for input data
   if (!(node == null || node.constructor == Node))
     throw new Error("Input must be a node.");
 
+  // If there is not next node, there is not cycle
   if (node.next === null) return false;
 
+  // Setting the pointers
   let slowPointer = node;
   let fastPointer = node.nextNode;
+
   do {
     try {
+      // Setting pointers for next cycle
       slowPointer = slowPointer.nextNode;
       fastPointer = fastPointer.nextNode.nextNode;
     } catch (error) {
+      // If there is a error, is because we reached the end of
+      // the linked list and the pointers cannot store the nextNode
       return false;
     }
+    // Checking if the pointers are the same
     if (slowPointer === fastPointer) return true;
+
+    //Looping till there are more nextNodes
   } while (fastPointer != null && fastPointer.nextNode != null);
 
   return false;
