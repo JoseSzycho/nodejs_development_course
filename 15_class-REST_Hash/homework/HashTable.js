3d0086b6e9aef5450f0bad3ef65e40735ea475fe
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-13 09:46:47 -0300

    Add insert method

--- 15_class-REST_Hash/homework/HashTable.js
+++ 15_class-REST_Hash/homework/HashTable.js
@@ -1,3 +1,43 @@
+const linkedListPath =
+  "../../14_class-Data_structure/homework/01_data_structure_implementations/linked_list/LinkedList";
 const { hash } = require("./hash");
+const { LinkedList } = require(linkedListPath);
 
-class HashTable {}
+class HashTable {
+  #table;
+  constructor() {
+    this.#table = new Map();
+  }
+
+  #hash(key) {
+    return hash(key);
+  }
+
+  insert(key, value) {
+    const hashedKey = this.#hash(key);
+
+    if (this.#table.has(hashedKey)) {
+      this.#table.get(hashedKey).insertRear(value);
+    } else {
+      const linkedList = new LinkedList();
+      linkedList.insertRear(value);
+      this.#table.set(hashedKey, linkedList);
+    }
+  }
+
+  get(key) {
+    const hashedKey = this.#hash(key);
+
+    const value = this.#table.get(hashedKey);
+
+    
+  }
+}
+
+const table = new HashTable();
+
+table.insert("a", 2);
+table.insert("b", 2);
+table.insert("b", 3);
+table.insert("b", 4);
+table.getTable();
