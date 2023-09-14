8566ff496d2bd8511484f1446b4e03a5ef8db3d3
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-14 19:33:08 -0300

    Modify to add avalanche effect

--- 15_class-REST_Hash/homework/hash.js
+++ 15_class-REST_Hash/homework/hash.js
@@ -2,7 +2,11 @@ const hash = (key) => {
   if (typeof key != "string") throw new Error("Key must be String type.");
   let keyCharCode = "";
 
-  for (char of key) keyCharCode = keyCharCode.concat(char.charCodeAt(0));
+  for (char of key) {
+    keyCharCode = String(keyCharCode.concat(char.charCodeAt(0) << 2));
+  }
+
+  keyCharCode = keyCharCode.split("").reverse("").join("");
 
   return BigInt(keyCharCode) % BigInt(Number.MAX_VALUE);
 };
