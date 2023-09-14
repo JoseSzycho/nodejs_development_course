40b6d6d7967e589ceb07bbce7f9af3db9ebf67c5
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-14 20:02:11 -0300

    Add comments

--- 15_class-REST_Hash/homework/HashTable.js
+++ 15_class-REST_Hash/homework/HashTable.js
@@ -3,16 +3,30 @@ const linkedListPath =
 const { hash } = require("./hash");
 const { LinkedList } = require(linkedListPath);
 
+/**
+ * Class that creates and manages a hash table.
+ */
 class HashTable {
   #table;
   constructor() {
+    // Stores the hash table
     this.#table = new Map();
   }
-
+  /**
+   * Returns the hashed value of a key.
+   * @param {String} key The key
+   * @returns {BigInt} The hashed value
+   * @private
+   */
   #hash(key) {
     return hash(key);
   }
-
+  /**
+   * Inserts a pair of key-value in the hash table.
+   * @param {String} key The key
+   * @param {*} value The value
+   * @returns {Boolean} __true__
+   */
   insert(key, value) {
     const hashedKey = this.#hash(key);
 
@@ -27,6 +41,11 @@ class HashTable {
     return true;
   }
 
+  /**
+   * Gets the value/values stored in a key.
+   * @param {String} key The key
+   * @returns {( * | Array)} The value, an array if multiple values.
+   */
   get(key) {
     const hashedKey = this.#hash(key);
 
@@ -43,11 +62,22 @@ class HashTable {
     return [...value];
   }
 
+  /**
+   * Delete the value / values  stored in a key.
+   * @param {String} key The key
+   * @returns {Boolean} __true__ if deleted, __false__ if key is not stored.
+   */
   deleteByKey(key) {
     const hashedKey = this.#hash(key);
     return this.#table.delete(hashedKey);
   }
 
+  /**
+   * Delete a certain value stored in a key.
+   * @param {String} key The key
+   * @param {*} value The value
+   * @returns {Boolean} __true__ if deleted, __false__ if key-value is not stored.
+   */
   deleteByKeyValue(key, value) {
     const hashedKey = this.#hash(key);
 
