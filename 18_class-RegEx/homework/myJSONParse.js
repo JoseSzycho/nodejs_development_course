8521e48eef99321ad6ebb47293cdb63495faa8f3
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-30 17:09:04 -0300

    Add comments

--- 18_class-RegEx/homework/myJSONParse.js
+++ 18_class-RegEx/homework/myJSONParse.js
@@ -1,16 +1,25 @@
 const { JSONtokenization } = require("./JSONtokenization");
-
+/** Parses a JSON string, constructing the JavaScript value or object described by the string.
+ *
+ * It works by tokenizing the JSON string, after that, it implements an algorithm
+ * that process each token in order to decide if the token is a value to add,
+ * if is a the aperture of an array or object, or if it is the closing of
+ * an array or object.
+ *
+ * @param {String} JSONString The string
+ * @returns {any} The parsed value or object
+ */
 const myJSONParse = (JSONString) => {
   // Used for the constructing the parsed JSON
   let parsedJSON;
   // Stores next token to parse
   let token;
-
   // Stores actual position of parsedJSON for handling nested objects
   let objectStack = [];
-  // Store actual state of parsedJSON, if we are inside a object or array
+  // Store actual state of parsedJSON, in order to know if we are
+  // into a object or into an array
   let stateStack = [];
-  // Actual object key;
+  // Actual object key
   let key;
   // Tokenized JSON String to process and parse
   const tokenizedString = JSONtokenization(JSONString);
@@ -18,7 +27,8 @@ const myJSONParse = (JSONString) => {
   // If length === 1, there is a primitive data type
   if (tokenizedString.length === 1) return tokenizedString[0];
 
-  // Analyzing first token
+  // Analyzing first token, in order to know if we
+  // start from a object or a array
   token = tokenizedString.shift();
   if (token === "{") {
     parsedJSON = {};
@@ -29,83 +39,77 @@ const myJSONParse = (JSONString) => {
     stateStack.push("array");
   }
 
+  // Parsing elements till all elements are parsed
   while (tokenizedString.length > 0) {
+    // Getting token
     token = tokenizedString.shift();
 
     switch (token) {
-      case ",":
-      case ":":
+      case ",": // ignore, skip to next cycle
+      case ":": // ignore, skip to next cycle
         break;
-      case "{":
+      case "{": // Adding a nested object
+        // If we are into a array
         if (stateStack[stateStack.length - 1] === "array") {
-          parsedJSON.push({});
-          objectStack.push(parsedJSON);
-          stateStack.push("object");
+          parsedJSON.push({}); //
+          objectStack.push(parsedJSON); // storing actual position
+          stateStack.push("object"); // storing actual state
           parsedJSON = parsedJSON[parsedJSON.length - 1];
           break;
         }
 
+        // If we are into a object
         if (stateStack[stateStack.length - 1] === "object") {
           parsedJSON[key] = {};
-          objectStack.push(parsedJSON);
-          stateStack.push("object");
+          objectStack.push(parsedJSON); // storing actual position
+          stateStack.push("object"); // storing actual state
           parsedJSON = parsedJSON[key];
           break;
         }
 
-      case "[":
+      case "[": // Adding a nested array
+        // If we are into an array
         if (stateStack[stateStack.length - 1] === "array") {
           parsedJSON.push([]);
-          objectStack.push(parsedJSON);
-          stateStack.push("array");
+          objectStack.push(parsedJSON); // storing actual position
+          stateStack.push("array"); // storing actual state
           parsedJSON = parsedJSON[parsedJSON.length - 1];
           break;
         }
 
+        // If we are into a object
         if (stateStack[stateStack.length - 1] === "object") {
           parsedJSON[key] = [];
-          objectStack.push(parsedJSON);
-          stateStack.push("array");
+          objectStack.push(parsedJSON); // storing actual position
+          stateStack.push("array"); // storing actual state
           parsedJSON = parsedJSON[key];
           break;
         }
 
-      case "}":
-        if (
-          stateStack[stateStack.length - 1] === "object" &&
-          objectStack.length > 0
-        ) {
-          parsedJSON = objectStack.pop();
-          stateStack.pop();
+      case "}": // jumping outside a nested object
+      case "]": // jumping outside a nested array
+        if (objectStack.length > 0) {
+          parsedJSON = objectStack.pop(); // obtaining last parsedJSON position
+          stateStack.pop(); // obtaining past parsedJSON state
         }
         break;
-      case "]":
-        if (
-          stateStack[stateStack.length - 1] === "array" &&
-          objectStack.length > 0
-        ) {
-          parsedJSON = objectStack.pop();
-          stateStack.pop();
-        }
-        break;
-      default:
-        // If it a key
+      default: // If here, we have a key or a value to add
+        // Checking if we should get a key
         if (tokenizedString[0] === ":") {
-          // Skips to nex cycle
-          key = token.slice(1, -1);
-          continue;
+          key = token.slice(1, -1); // '"key"' converts into "key"
+          continue; // Skips to nex cycle
         }
 
+        // If we are into an object, add value by key
         if (stateStack[stateStack.length - 1] === "object")
           parsedJSON[key] = token;
 
+        // If we are into an array, push value
         if (stateStack[stateStack.length - 1] === "array")
           parsedJSON.push(token);
         break;
     }
   }
-
-  console.log(parsedJSON);
   return parsedJSON;
 };
 
