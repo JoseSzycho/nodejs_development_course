6e3718bf2a167ae1d8a56c475fa14ccc934ead1c
Jose Szychowski <jszychowski.laba@solvd.com>
2023-09-30 12:46:10 -0300

    Add support for array and nested array

--- 18_class-RegEx/homework/myJSONParse.js
+++ 18_class-RegEx/homework/myJSONParse.js
@@ -3,23 +3,23 @@ const { JSONtokenization } = require("./JSONtokenization");
 const myJSONParse = (JSONString) => {
   // Used for the constructing the parsed JSON
   let parsedJSON;
-
+  // Stores next token to parse
+  let token;
   /* Stores the actual state of the parsedJSON,
     <"array">  indicates we are in a array, so primitive must be pushed
     <"object"> indicates we are in a object, so primitive is added after a key 
   */
   let inArrayOrObject = false;
-
+  // Stores actual position of parsedJSON for handling nested objects
+  let stack = [];
   // Tokenized JSON String to process and parse
   const tokenizedString = JSONtokenization(JSONString);
 
   // If length === 1, there is a primitive data type
   if (tokenizedString.length === 1) return tokenizedString[0];
 
-  let token;
-
+  // Analyzing first token
   token = tokenizedString.shift();
-
   if (token === "{") {
     parsedJSON = {};
     inArrayOrObject = "object";
@@ -29,5 +29,38 @@ const myJSONParse = (JSONString) => {
     inArrayOrObject = "array";
   }
 
-  do {} while (tokenizedString.length > 0);
+  while (tokenizedString.length > 0) {
+    
+    token = tokenizedString.shift();
+
+    switch (token) {
+      case "{":
+        break;
+      case "[":
+        if (inArrayOrObject === "array") {
+          parsedJSON.push([]);
+          stack.push(parsedJSON);
+          parsedJSON = parsedJSON[parsedJSON.length - 1];
+        }
+        break;
+      case ",":
+        break;
+      case ":":
+        break;
+      case "}":
+        break;
+      case "]":
+        if (inArrayOrObject === "array" && stack.length > 0) {
+          parsedJSON = stack.pop();
+        }
+        break;
+      default:
+        if (inArrayOrObject === "array") parsedJSON.push(token);
+        break;
+    }
+  }
+
+  return parsedJSON;
 };
+
+module.exports = { myJSONParse };
