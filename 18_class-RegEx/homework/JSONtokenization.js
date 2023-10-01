b16c75585d975e255274bf0e30cbd19c74dfcccf
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-01 02:03:46 -0300

    Add identation check and single value check for errors

--- 18_class-RegEx/homework/JSONtokenization.js
+++ 18_class-RegEx/homework/JSONtokenization.js
@@ -1,15 +1,19 @@
+const { isValidToken } = require("./utils/isValidToken");
+
 /**
  * Perform the tokenization of a structured JSON string.
  *
  * All primitive values are parsed into its correct data
  * type, even if the primitive is part of an object or array.
- * 
- * To differentiate a key of a string, strings are inside "<...>" and 
+ *
+ * To differentiate a key of a string, strings are inside "<...>" and
  * keys are inside '"<...>"'
  * @param {string} JSONstring The JSON string
  * @returns {array} The tokenized JSON string
  */
 const JSONtokenization = (JSONstring) => {
+  const checkToken = isValidToken();
+
   /* regEx explanation
     matching object keys    (?<key>"[a-zA-Z]+")(?=:) 
     matching boolean        (?<boolean>true|false)
@@ -25,25 +29,26 @@ const JSONtokenization = (JSONstring) => {
   const regEx =
     /(?<object>\{|\})|(?<key>"[a-zA-Z]+")(?=:)|(?<string>".+?")|(?<separator>:|,)|(?<boolean>(true|false))|(?<number>-?\d+\.?\d*)|(?<array>\[|\])|(?<null>null)/g;
 
-  let match;
+  let token;
   const tokens = [];
 
   // Tokenizing the JSON string
-  while ((match = regEx.exec(JSONstring))) {
+  while ((token = regEx.exec(JSONstring))) {
+    checkToken(token);
     // Using named groups for parsing the primitive values
-    if (match.groups.number) tokens.push(Number(match[0]));
-    if (match.groups.boolean)
-      tokens.push(match[0] === "true" ? true : false);
-    if (match.groups.string) tokens.push(String(match[0].slice(1,-1)));
-    if (match.groups.null) tokens.push(null);
+    if (token.groups.number) tokens.push(Number(token[0]));
+    if (token.groups.boolean) tokens.push(token[0] === "true" ? true : false);
+    if (token.groups.string) tokens.push(String(token[0].slice(1, -1)));
+    if (token.groups.null) tokens.push(null);
 
     // Using named groups for completing tokenization
-    if (match.groups.object) tokens.push(match[0]);
-    if (match.groups.array) tokens.push(match[0]);
-    if (match.groups.key) tokens.push(match[0]);
-    if (match.groups.separator) tokens.push(match[0]);
+    if (token.groups.object) tokens.push(token[0]);
+    if (token.groups.array) tokens.push(token[0]);
+    if (token.groups.key) tokens.push(token[0]);
+    if (token.groups.separator) tokens.push(token[0]);
   }
 
+  checkToken(null, true);
   // tokenized JSON string
   return tokens;
 };
