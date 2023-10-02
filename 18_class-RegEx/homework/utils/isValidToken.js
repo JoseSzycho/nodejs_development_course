186dfd64f19282432787711dba7247d2df2721c4
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-01 23:40:46 -0300

    Add comments

--- 18_class-RegEx/homework/utils/isValidToken.js
+++ 18_class-RegEx/homework/utils/isValidToken.js
@@ -1,8 +1,18 @@
+/**
+ * Higher Order Function sed by `../JSONtokenization.js` for determining
+ * if a token is valid or not. If it is not valid, throws an error showing
+ * the description of the error.
+ * @param {Any} token
+ * @returns
+ */
 const isValidToken = (token) => {
+  // Store the tokens in order to access to a previous one
   let tokens = [];
+  // Used for determining if nested objects are well created
   let bracesBalanceCount = 0;
   let bracketsBalanceCount = 0;
 
+  // Function that looks for indentation errors
   const checkIndentations = () => {
     if (bracesBalanceCount > 0) throw new Error('Missing "}"');
     if (bracesBalanceCount < 0) throw new Error('Missing "{"');
@@ -10,6 +20,8 @@ const isValidToken = (token) => {
     if (bracketsBalanceCount < 0) throw new Error('Missing "["');
   };
 
+  // Function that checks if is a valid single value in the case
+  // the JSON string do not have any array or object.
   const checkForSingleValue = () => {
     if (tokens.length === 0) throw new Error("Expected JSON input.");
     if (tokens.length === 1) {
@@ -26,11 +38,13 @@ const isValidToken = (token) => {
     }
   };
 
+  // Checks if the key or property name is correct
   const checkKeyPosition = (previousToken) => {
     if (!(previousToken[0] === "," || previousToken[0] === "{"))
       throw new Error(`Invalid key position at index ${previousToken.index}`);
   };
 
+  // Checks if the position of "," or ":" is correct
   const checkSeparatorPosition = (token, previousToken) => {
     if (token[0] === ":" && previousToken.groups.key === undefined) {
       throw new Error(`Invalid key at position ${previousToken.index}`);
@@ -51,12 +65,14 @@ const isValidToken = (token) => {
     }
   };
 
+  // Checks if a value is in correct position
   const checkValuePosition = (previousToken) => {
     if (!(previousToken.groups.separator || previousToken[0] === "[")) {
       throw new Error(`Invalid value at position ${previousToken.index + 1}`);
     }
   };
 
+  // Checks if "{ or } or [ or ]" positions are correct
   const checkObjectPosition = (token, previousToken) => {
     if (token[0] === "{" || token[0] === "[") {
       if (
@@ -88,18 +104,23 @@ const isValidToken = (token) => {
   };
 
   return (token, finalCheck = false) => {
+    // If the tokenization process is finished, performs final tests
     if (finalCheck) {
       checkForSingleValue();
       checkIndentations();
       return;
     }
+
+    // Storing token in order to analyze the previous ones
     tokens.push(token);
 
+    // Storing indentation levels for final check
     if (token[0] === "{") bracesBalanceCount++;
     if (token[0] === "[") bracketsBalanceCount++;
     if (token[0] === "}") bracesBalanceCount--;
     if (token[0] === "]") bracketsBalanceCount--;
 
+    // If there are no previous token
     if (tokens.length <= 1) return;
 
     const previousToken = tokens[tokens.length - 2];
